03c73d392382b3e98d3fd94fe5d634614389cbe4
Ivan Dustin B. Bilon <ivan22.dust@gmail.com>
2021-04-16 12:12:24 +0800

    Replace yargs with argparse and update package.json

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-require('./src/cli')()
+#!/usr/bin/env node
+const package     = require('./package.json')
+const argparse    = require('argparse')
+const load        = require('./src/load')
+const interactive = require('./src/interactive')
+
+function main() {
+    let args        = parse()
+    let directories = args.directory
+    let manuscripts = load(directories)
+    interactive(manuscripts)
+}
+
+function parse() {
+    let { description, version } = package
+    let parser = new argparse.ArgumentParser({ description })
+    parser.add_argument('-V', '--version', { help: 'show version information and exit', action: 'version', version })
+    parser.add_argument('directory',       { nargs: '+', help: 'input directory that contains the manuscripts' })
+    parser.add_argument('-o',              { metavar: 'OUTPUT', help: 'output directory of the collated manuscripts' })
+    return parser.parse_args()
+}
+
+main()
